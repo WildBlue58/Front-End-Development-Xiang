e70794ab99c08e6cbad8e7506b5c352920e97a97
WildBlue <2398205893@qq.com>
2026-01-28 20:02:47 +0800

    feat(zijie/LRU): 新增 LRU 缓存算法的 JavaScript 实现
    添加两个文件，包含多个 LRU 缓存类的实现。主要使用 JavaScript 的 Map 数据结构，利用其维护插入顺序的特性来实现 LRU（最近最少使用）的淘汰机制。get 操作会将访问的键移至最新，put 操作在超出容量时自动移除最旧的键。

---
+++ zijie/LRU/1.js
@@ -0,0 +1,143 @@
+class LRU{
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1
+        
+        const value = this.map.get(key)
+        this.map.delete(key)
+        this.map.set(key, value)
+        
+        return value
+    }
+
+    put(key, value) {
+        if (this.map.has(key)) {
+           this.map.delete(key) 
+        }
+
+        this.map.set(key, value)
+        
+        if (this.map.size > this.capacity) {
+            this.map.delete(this.map.keys().next().value)
+        }
+    }
+}
+
+class LRU1{
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+
+    get(key) {
+        if (!this.map.has(key)) return -1
+        
+        const value = this.map.get(key)
+        this.map.delete(key)
+        this.map.set(key, value)
+        
+        return value
+    }
+
+    put(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key)
+        }
+
+        this.map.set(key, value)
+        
+        if (this.map.size > this.capacity) {
+            this.map.delete(this.map.keys().next().value)
+        }
+    }
+}
+
+class LRU2{
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1
+        
+        const value = this.map.get(key)
+        this.map.delete(key)
+        this.map.set(key, value)
+        
+        return value
+    }
+
+    set(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key)
+        }
+
+        this.map.set(key, value)
+        
+        if (this.map.size > this.capacity) {
+            this.map.delete(this.map.keys().next().value)
+        }
+    }
+}
+
+class LRU3{
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1
+        
+        const value = this.map.get(key)
+        this.map.delete(key)
+        this.map.set(key,value)
+
+        return value
+    }
+
+    set(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key)
+        }
+
+        this.map.set(key, value)
+        
+        if (this.map.size > this.capacity) {
+            this.map.delete(this.map.keys().next().value)
+        }
+    }
+}
+
+class LRU4{
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+    get(key) {
+        if (!this.map.has(key)) return -1
+        
+        const value = this.map.set(key)
+        this.map.delete(key)
+        this.map.set(key,value)
+    }
+
+    set(key, value) {
+        if (this.map.has(key)) {
+            this.map.delete(key)
+        }
+
+        this.map.set(key, value)
+        
+        if (this.map.size > this.capacity) {
+            this.map.delete(this.map.keys().next().value)
+        }
+    }
+}
